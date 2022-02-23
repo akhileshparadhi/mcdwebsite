461c07f5695faffc99113ec6ef9022705f3e720e
akhileshparadhi <akhilesh.paradhi.121@gmail.com>
2022-02-23 20:44:55 +0530

    Upload whole code

---
+++ Test/menu.js
@@ -0,0 +1,11 @@
+function showcontent(){
+
+var x = document.getElementById("menu-1");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+
+
+};
